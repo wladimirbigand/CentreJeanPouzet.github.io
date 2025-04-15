d652fd7ba01eaf7e3c6703bbf1406594b46b6370
Wladimir <bigand@et.esiea.fr>
2025-04-15 15:59:20 +0200

    Ajout du Footer.php et du require dans chaque page

--- HTML/Colos.php
+++ HTML/Colos.php
@@ -145,22 +145,8 @@
         </section>
 
     </main>
-    <footer>
-        <div class="footer-left">
-            <a href="Mentions Legales.php">Mentions légales</a>
-            <a href="Politique de Confidentialite.php">Politique de confidentialité</a>
-        </div>
-        <div class="footer-center">
-            <a href="../HTML/Test.html">Admin <span>&#128274;</span></a>
-            ©2024 ColoConnect | Tous droits réservés
-        </div>
-        <div class="footer-right">
-            <div class="contact"><a href="Nous Contacter.php"><b>Nous contacter</b></a></div>
-            <div class="social-icons">
-                <a href="https://www.facebook.com/centrejeanpouzet/"><img src="../Images/Logo/Icone_Facebook.svg" alt="Facebook"></a>
-                <a href="https://www.instagram.com/centrejeanpouzet/?hl=fr"><img src="../Images/Logo/Icone_Instagram.svg" alt="Instagram"></a>
-            </div>
-        </div>
-    </footer>
+<?php
+require_once '../Includes/Footer.php';
+?>
 </body>
 </html>
