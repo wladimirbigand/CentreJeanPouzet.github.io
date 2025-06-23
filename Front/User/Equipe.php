7a42d0d7196321b4a1f9e81c2c42d94d8534073d
Wladimir <bigand@et.esiea.fr>
2025-06-23 16:45:53 +0200

    Correction de "Nous contacter" et des boutons du caroussel.

--- Front/User/Equipe.php
+++ Front/User/Equipe.php
@@ -69,12 +69,14 @@ require_once '../Includes/Header.php';
             </div>
 
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselEquipe" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon color" aria-hidden="true"></span>
-                <span class="visually-hidden">Précédent</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+                    <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
+                </svg>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselEquipe" data-bs-slide="next">
-                <span class="carousel-control-next-icon color" aria-hidden="true"></span>
-                <span class="visually-hidden">Suivant</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                    <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
+                </svg>
             </button>
         </div>
     </div>
